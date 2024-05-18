32c6e4fdb2d6081d56567d284a14101887bd6214
JPapECE <157026044+JPapECE@users.noreply.github.com>
2024-05-18 18:02:25 +0300

    Add files via upload

--- db2024_views.sql
+++ db2024_views.sql
@@ -1,3 +1,8 @@
+DROP VIEW IF EXISTS Cook_Recipe_Count;
+DROP VIEW IF EXISTS Cook_Episode_Count;
+DROP  VIEW IF EXISTS Judge_Episode_Count_per_Year;
+DROP  VIEW IF EXISTS Episode_Combo_Total_Rating;
+
 #a view to see how many recipes each cook has
 CREATE VIEW Cook_Recipe_Count AS
 SELECT
@@ -44,49 +49,22 @@ JOIN
 	Episode e ON ej.episode_id = e.episode_id
 GROUP BY
     c.cook_id,e.year;
+    
 CREATE VIEW Episode_Combo_Total_Rating AS
 SELECT
-    ec.cook_id,
+    ec.cook_id AS cook_id,
     CONCAT(c.first_name, ' ', c.last_name) AS cook_name,
-    c.chef_status,
-    ec.episode_id,
+    ec.episode_combo_id AS episode_combo_id,
+    c.chef_status AS chef_status,
+    ec.episode_id AS episode_id,
     SUM(r.rating) AS total_rating
-
 FROM
 	Cook c
 JOIN
     Episode_Combo ec ON c.cook_id = ec.cook_id
 JOIN
     Rates r ON ec.episode_combo_id = r.episode_combo_id
-GROUP BY
-    ec.episode_id
-ORDER BY
-    total_rating DESC,
-    chef_status DESC
-    ;
+GROUP BY 
+	ec.episode_combo_id
+ORDER BY ec.episode_id,total_rating DESC,chef_status DESC;
 
-CREATE VIEW Winner_Cook_Episode AS
-SELECT
-    episode_id,
-    cook_id,
-    cook_name,
-    chef_status,
-    MAX(total_rating) AS max_total_rating
-FROM
-   Episode_Combo_Total_Rating 
-GROUP BY
-    episode_id;
-
-DELIMITER //
-CREATE PROCEDURE insert_winner_cook_id()
-BEGIN
-    UPDATE Episode
-    SET winner_cook_id = (
-        SELECT cook_id
-        FROM Winner_Cook_Episode
-        WHERE episode_id = NEW.episode_id
-    )
-    WHERE episode_id = NEW.episode_id;
-END;
-//
-DELIMITER ;
