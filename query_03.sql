41e191591da36df0e75a17a04d753f6cea88ada8
JPapECE <157026044+JPapECE@users.noreply.github.com>
2024-05-18 15:49:16 +0300

    Update query_03.sql

--- query_03.sql
+++ query_03.sql
@@ -1,4 +1,5 @@
 use db2024;
+DROP VIEW IF EXISTS Cook_Recipe_Count;
 CREATE VIEW Cook_Recipe_Count AS
 SELECT
     c.cook_id,
@@ -10,13 +11,10 @@ FROM
 JOIN
     Recipe_Cook rc ON c.cook_id = rc.cook_id
 GROUP BY
-    c.cook_id
-
-
+    c.cook_id;
+#version 1
 SELECT
-    cook_id,
-    cook_name,
-    recipe_count
+	*
 FROM
     Cook_Recipe_Count
 WHERE
@@ -30,3 +28,13 @@ AND
         WHERE
             age < 30
     );
+
+#version 2
+SELECT
+	*
+FROM
+    Cook_Recipe_Count
+WHERE
+    age < 30
+ORDER BY recipe_count DESC 
+LIMIT 8;
