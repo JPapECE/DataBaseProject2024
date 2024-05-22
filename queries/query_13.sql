0c00f2bcd2cb24a7b2d466d580b7feecbdd89c48
efrosiniiliadi <164185190+efrosiniiliadi@users.noreply.github.com>
2024-05-22 10:38:35 +0300

    Add files via upload

--- queries/query_13.sql
+++ queries/query_13.sql
@@ -1,38 +1,30 @@
 use db2024;
 
 SELECT 
-	ep_status_sum.episode_id
+	final.episode_id
 FROM
 	(SELECT 
-		ep_status_num.episode_id,
-		sum(number_status) as sum_status
+		ecj_status.episode_id,
+		sum(chef_status) as sum_status
 	FROM
 		(SELECT
-			ecj_status.episode_id, l.number_status 
-		FROM
-			(SELECT
-				ecj.episode_id, ecj.cook_id, c.chef_status
-			FROM	
-				Cook c, 
+			ecj.episode_id, c.chef_status
+		FROM	
+			Cook c
+			JOIN 
 				((SELECT episode_id, cook_id
 				FROM Episode_Combo)
 				UNION ALL
 				(SELECT episode_id, judge_id
 				FROM Episode_Judge)) ecj
-			WHERE
+			ON
 				c.cook_id = ecj.cook_id
-			) ecj_status
-		JOIN
-			ChefStatus l ON ecj_status.chef_status = l.chef_status
-		) ep_status_num
-    
+		) ecj_status
 	GROUP BY
-		ep_status_num.episode_id
-        
-	) ep_status_sum
-
+		ecj_status.episode_id
+	) final
 ORDER BY
-	ep_status_sum.sum_status ASC
+	final.sum_status ASC
 LIMIT 1;
 
 
