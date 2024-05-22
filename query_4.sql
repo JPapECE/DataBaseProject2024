ffcd0bb12cf856105c2c03611bdb911bb4b880ff
fragkiskakim <163539472+fragkiskakim@users.noreply.github.com>
2024-05-22 09:56:54 +0300

    Delete query_4.sql

--- query_4.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    c.cook_id,
-    CONCAT(c.first_name, ' ', c.last_name) AS judge_name
-FROM
-    Cook c
-LEFT JOIN
-    Episode_Judge ej ON c.cook_id = ej.judge_id
-WHERE
-    ej.episode_judge_id IS NULL;
