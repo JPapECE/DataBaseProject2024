07c8ee7c75763d2c0ca1463389eaf03c8f8a0ada
fragkiskakim <163539472+fragkiskakim@users.noreply.github.com>
2024-05-26 23:52:42 +0300

    Update Queries.sql

--- sql code/Queries.sql
+++ sql code/Queries.sql
@@ -185,6 +185,7 @@ SELECT * FROM information_schema.optimizer_trace;
 SET optimizer_trace="enabled=off";
 
 #Indexes 
+#uncomment to run the query plan
 #CREATE INDEX idx_recipe_label_recipe_id ON Recipe_Label(recipe_id);
 #CREATE INDEX idx_recipe_label_label_id ON Recipe_Label(label_id);
 #CREATE INDEX idx_episode_combo_recipe_id ON Episode_Combo(recipe_id);
@@ -290,6 +291,7 @@ SELECT * FROM information_schema.optimizer_trace;
 SET optimizer_trace="enabled=off";
 
 #Indexes for Recipe_Equipment table
+#uncomment to run the query plan
 #CREATE INDEX idx_recipe_equipment_recipe_id ON Recipe_Equipment(recipe_id);
 #CREATE INDEX idx_recipe_equipment_equipment_id ON Recipe_Equipment(equipment_id);
 #CREATE INDEX idx_episode_combo_recipe_id ON Episode_Combo(recipe_id);
